d4224d26f37dbac7492c51f6f5eec2a8bda71a23
khalilbouderra <anamusslim2009@gmail.com>
2016-06-28 18:17:53 +0000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Amawal
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Search_TXT = new System.Windows.Forms.TextBox();
+            this.Result = new System.Windows.Forms.DataGridView();
+            this.Search_btn = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.text = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.bugToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.Result)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Search_TXT
+            // 
+            this.Search_TXT.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.Search_TXT.Location = new System.Drawing.Point(84, 30);
+            this.Search_TXT.Name = "Search_TXT";
+            this.Search_TXT.Size = new System.Drawing.Size(629, 20);
+            this.Search_TXT.TabIndex = 0;
+            this.Search_TXT.TextChanged += new System.EventHandler(this.Search_TXT_TextChanged);
+            // 
+            // Result
+            // 
+            this.Result.AllowUserToAddRows = false;
+            this.Result.AllowUserToDeleteRows = false;
+            this.Result.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.Result.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.Result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Result.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.Result.Location = new System.Drawing.Point(0, 0);
+            this.Result.Name = "Result";
+            this.Result.RowHeadersVisible = false;
+            this.Result.RowHeadersWidth = 20;
+            this.Result.RowTemplate.Height = 30;
+            this.Result.RowTemplate.ReadOnly = true;
+            this.Result.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.Result.Size = new System.Drawing.Size(721, 379);
+            this.Result.TabIndex = 1;
+            this.Result.DoubleClick += new System.EventHandler(this.Result_DoubleClick);
+            // 
+            // Search_btn
+            // 
+            this.Search_btn.Location = new System.Drawing.Point(3, 27);
+            this.Search_btn.Name = "Search_btn";
+            this.Search_btn.Size = new System.Drawing.Size(75, 23);
+            this.Search_btn.TabIndex = 3;
+            this.Search_btn.Text = "Search";
+            this.Search_btn.UseVisualStyleBackColor = true;
+            this.Search_btn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.splitContainer1.Location = new System.Drawing.Point(3, 56);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.Result);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.text);
+            this.splitContainer1.Size = new System.Drawing.Size(721, 494);
+            this.splitContainer1.SplitterDistance = 379;
+            this.splitContainer1.TabIndex = 4;
+            // 
+            // text
+            // 
+            this.text.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.text.Font = new System.Drawing.Font("Tahoma", 20F);
+            this.text.Location = new System.Drawing.Point(0, 0);
+            this.text.Multiline = true;
+            this.text.Name = "text";
+            this.text.Size = new System.Drawing.Size(721, 111);
+            this.text.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.helpToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(725, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.bugToolStripMenuItem});
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(49, 20);
+            this.helpToolStripMenuItem.Text = "Help?";
+            // 
+            // bugToolStripMenuItem
+            // 
+            this.bugToolStripMenuItem.Name = "bugToolStripMenuItem";
+            this.bugToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.bugToolStripMenuItem.Text = "Bug";
+            this.bugToolStripMenuItem.Click += new System.EventHandler(this.bugToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.Search_btn;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(725, 556);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.Search_btn);
+            this.Controls.Add(this.Search_TXT);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Tahoma", 8F);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ⴰⵎⴰⵡⴰⵍ ⵎⵄⵜⵓⴱ ⵍⵓⵏⴰⵙ || قاموس معتوب لوناس";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.Result)).EndInit();
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        private System.Windows.Forms.TextBox Search_TXT;
+        private System.Windows.Forms.DataGridView Result;
+        private System.Windows.Forms.Button Search_btn;
+
+        #endregion
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.TextBox text;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem bugToolStripMenuItem;
+
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SQLite;
+namespace Amawal
+{
+    public partial class Form1 : Form
+    {
+        #region Declaration
+        //public Form1()
+        //{
+        //    InitializeComponent();
+        //}
+
+        Chars Validation(string Text)
+        {
+            foreach (char item in Search_TXT.Text)
+                foreach (char tiitem in tifinagh_chars)
+                    if (item == tiitem)
+                        return Chars.Tifinagh;
+                    else
+                        foreach (char aritem in arabic_chars)
+                            if (item == aritem)
+                                return Chars.Arabic;
+            return Chars.None;
+        }
+        enum Chars
+        {
+            Tifinagh, Arabic, None
+        }
+
+        SQLiteConnection cn = new SQLiteConnection(@"Data Source=C:\Users\Khalil Bouderra\Desktop\Dictionary_0.6.1.db;Version=3;New=False;Compress=True;");
+        char[] tifinagh_chars = { 'ⴰ', 'ⵣ', 'ⴻ', 'ⵔ', 'ⵜ', 'ⵢ', 'ⵓ', 'ⵉ', 'ⵄ', 'ⵃ', 'ⵇ', 'ⵙ', 'ⴷ', 'ⴼ', 'ⴳ', 'ⵀ', 'ⵊ', 'ⴽ', 'ⵍ', 'ⵎ', 'ⵡ', 'ⵅ', 'ⵛ', 'ⵖ', 'ⴱ', 'ⵏ' };
+        char[] arabic_chars = { 'ض', 'ص', 'ث', 'ق', 'ف', 'غ', 'ع', 'ه', 'خ', 'ح', 'ج', 'د', 'ذ', 'ش', 'س', 'ي', 'ب', 'ل', 'ا', 'ت', 'ن', 'م', 'ك', 'ط', 'ئ', 'ء', 'ؤ', 'ر', 'ل', 'ا', 'ى', 'ة', 'و', 'ز', 'ظ', 'أ', 'إ' };
+        string arabic, arabic_meaning, tifinagh_in_arabic, tifinagh;
+        TextBox t = new TextBox();
+        #endregion
+
+        public Form1()
+        {
+            InitializeComponent();
+            /*
+            SQLiteCommand cmd = new SQLiteCommand();
+            cmd.CommandText = "Select * from words";
+            cmd.Connection = cn;
+            DataTable dt = new DataTable();
+            cn.Open();
+            try
+            {
+                dt.Load(cmd.ExecuteReader());
+            }
+            catch (SQLiteException ex) { }
+            finally
+            {
+                cn.Close();
+            }
+
+            Search_TXT.AutoCompleteMode = AutoCompleteMode.Suggest;
+            Search_TXT.AutoCompleteSource = AutoCompleteSource.CustomSource;
+            AutoCompleteStringCollection col = new AutoCompleteStringCollection();
+            for (int i = 0; i < dt.Rows.Count; i++)
+                col.Add(dt.Rows[i]["arabic"].ToString() + " : " + dt.Rows[i]["tifinagh"].ToString());
+            Search_TXT.AutoCompleteCustomSource = col;
+            #endregion
+            t.Font = new System.Drawing.Font("Tifinaghe-Izuren IRCAM", 50);
+            t.Multiline = true;
+            t.Height = panel1.Height;
+            t.Width = panel1.Width;
+            t.ReadOnly = true;
+            //t.BorderStyle = BorderStyle.None;
+            t.ScrollBars = ScrollBars.Vertical;*/
+            //t.Font = new System.Drawing.Font("Tifinaghe-Izuren IRCAM", 50);
+
+            text.ReadOnly = true;
+            //t.BorderStyle = BorderStyle.None;
+            text.ScrollBars = ScrollBars.Vertical;
+            ReDraw();
+            ReDraw();
+        }
+
+        private void Search_TXT_TextChanged(object sender, EventArgs e)
+        {
+            /*
+                        if (Search_TXT.Text.Length > 0)
+                        {
+                            string Word = string.Empty, table = string.Empty;
+                            Chars val = Validation(Search_TXT.Text);
+                            foreach (char item in Search_TXT.Text)
+                            {
+                                if (item == '\'')
+                                    Word += "\\'";
+                                else
+                                    Word += item;
+                            }
+                            SQLiteCommand cmd = new SQLiteCommand();
+                            if (val == Chars.Tifinagh)
+                            {
+                                table = "tifinagh";
+                                arabic = "ⵜⴰⵄⵔⴰⴱⵜ";
+                                arabic_meaning = "ⵜⴰⵄⵔⴰⴱⵜ ⵙ ⵉⵎⵃⴰⵙ";
+                                tifinagh_in_arabic = "ⵜⴰⵎⴰⵣⵉⵖⵜ ⵙ ⵜⵉⴼⵉⵏⴰⵖ ⵜⴰⵄⵔⴰⴱⵜ";
+                                tifinagh = "ⵜⴰⵎⴰⵣⵉⵖⵜ";
+                            }
+                            else if (val == Chars.Arabic)
+                            {
+                                table = "_arabic";
+                                arabic = "العربية";
+                                arabic_meaning = "العربية بالشكل";
+                                tifinagh_in_arabic = "الأمازيغية بحروف عربية";
+                                tifinagh = "الأمازيغية";
+                            }
+                            else
+                                return;
+                            cmd.CommandText = "Select arabic as [" + arabic + "]," +
+                                "arabic_meaning as [" + arabic_meaning + "]," +
+                                "tifinagh_in_arabic as [" + tifinagh_in_arabic + "]," +
+                                "tifinagh as [" + tifinagh + "] from words where " + table + " like '%" + Word + "%'";
+                            cmd.Connection = cn;
+                            DataTable dt = new DataTable();
+                            cn.Open();
+                            try { dt.Load(cmd.ExecuteReader()); }
+                            catch (SQLiteException ex) { }
+                            finally { cn.Close(); }
+                            Result.DataSource = dt;
+                            cn.Close();
+                        }
+                        */
+        }
+
+
+
+        private void Result_DoubleClick(object sender, EventArgs e)
+        {
+            //text.Text = "";
+            text.Text = Result.SelectedRows[0].Cells[0].Value.ToString() + " ( " +
+                Result.SelectedRows[0].Cells[1].Value.ToString() + " ) " + Environment.NewLine +
+                Result.SelectedRows[0].Cells[2].Value.ToString();
+            /*for (int i = 0; i < Result.SelectedRows[0].Cells.Count; i++)
+            {
+                if (i == 0)
+                    text.Text += Result.SelectedRows[0].Cells[i].Value.ToString();
+                else
+                    text.Text += " ( " + Result.SelectedRows[0].Cells[i].Value.ToString() + " ) ";
+            }*/
+            /*panel1.Controls.Clear();
+            panel1.Controls.Add(t);
+            panel1.Refresh();*/
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            ReDraw();
+        }
+        void ReDraw()
+        {
+            /*t.Width = panel1.Width;
+            panel1.Height = this.Height / 3;
+            panel1.Location = new Point(panel1.Location.X, (this.Height / 3) * 2);
+            Result.Location = new Point(Result.Location.X, Search_TXT.Location.Y + Search_TXT.Size.Height + 10);
+            Result.Size = new Size(this.Width - Result.Location.X - 20, this.Height - (Search_TXT.Location.Y + Search_TXT.Size.Height + 10 + panel1.Height) - 10);
+            Search_TXT.Location = new Point(10, 10);
+            Search_TXT.Size = new Size(this.Width - Search_TXT.Location.X - 20, Search_TXT.Height);
+            panel1.Size = new Size(this.Width - panel1.Location.X - 20, this.Height / 3);*/
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Search();
+        }
+        void Search()
+        {
+            if (Search_TXT.Text.Length > 0)
+            {
+                string Word = string.Empty;
+                List<string> Tables = new List<string> { };
+                Chars val = Validation(Search_TXT.Text);
+                foreach (char item in Search_TXT.Text)
+                {
+                    if (item == '\'')
+                        Word += "\\'";
+                    else
+                        Word += item;
+                }
+                SQLiteCommand cmd = new SQLiteCommand();
+                if (val == Chars.Tifinagh)
+                {
+                    //table = "tifinagh";
+                    arabic = "ⵜⴰⵄⵔⴰⴱⵜ";
+                    arabic_meaning = "ⵜⴰⵄⵔⴰⴱⵜ ⵙ ⵉⵎⵃⴰⵙⵏ";
+                    tifinagh_in_arabic = "ⵜⴰⵎⴰⵣⵉⵖⵜ ⵙ ⵜⵉⴼⵉⵏⴰⵖ ⵜⴰⵄⵔⴰⴱⵜ";
+                    tifinagh = "ⵜⴰⵎⴰⵣⵉⵖⵜ";
+                    Tables.Add("tifinagh");
+                }
+                else if (val == Chars.Arabic)
+                {
+                    //table = "_arabic";
+                    arabic = "العربية";
+                    arabic_meaning = "العربية بالشكل";
+                    tifinagh_in_arabic = "الأمازيغية بحروف عربية";
+                    tifinagh = "الأمازيغية";
+                    Tables.AddRange(new string[] { "_arabic", "_tifinagh_in_arabic", "arabic", "tifinagh_in_arabic" });
+                }
+                else
+                    return;
+                string QueryString = "Select tifinagh as [" + tifinagh + "]," +
+                    "tifinagh_in_arabic as [" + tifinagh_in_arabic + "], " +
+                    "arabic_meaning as [" + arabic_meaning + "] " +
+                    "from words where ";
+                for (int i = 0; i < Tables.Count; i++)
+                {
+                    if (i == 0)
+                        QueryString += Tables[i] + " like '%" + Word + "%'";
+                    else
+                        if (Tables.ToArray()[i].Length > 0)
+                            QueryString += " or " + Tables[i] + " like '%" + Word + "%'";
+                }
+                cmd.CommandText = QueryString;
+                cmd.Connection = cn;
+                DataTable dt = new DataTable();
+                cn.Open();
+                try { dt.Load(cmd.ExecuteReader()); }
+                catch (SQLiteException ex) { }
+                finally { cn.Close(); }
+                Result.DataSource = dt;
+                cn.Close();
+            }
+        }
+
+        private void bugToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://www.facebook.com/Akal.Awal.Afgan.Yaz");
+        }
+    }
+}
